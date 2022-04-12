7353a846905f4fa6c56ba3a93b8c4c6198687da5
SzabolcsMarton <maszabolcs@gmail.com>
2022-04-12 16:18:36 +0200

    add filter methods and refactor

--- src/main/java/activitytrackerdemo/menuitems/ListActivityMenuItem.java
+++ src/main/java/activitytrackerdemo/menuitems/ListActivityMenuItem.java
@@ -4,6 +4,7 @@ import activitytrackerdemo.Activity;
 import activitytrackerdemo.ActivityService;
 import activitytrackerdemo.ActivityType;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,36 +21,58 @@ public class ListActivityMenuItem implements MenuItem {
     @Override
     public void process(Scanner scanner) {
         allActivities = activityService.getAllActivities();
-
-        System.out.println("Tevékenységek listázása menüpont");
+        System.out.println("\tTevékenységek listázása menüpont");
         ensureActivitiesNotNullOrEmpty();
-
-        System.out.println("Szeretne szűrni a találatokat?");
-        System.out.println("y- Igen, n- Nem");
+        System.out.println("\tSzeretne szűrni a találatokat?");
+        System.out.println("\ty- Igen, n- Nem\t");
         String answer = scanner.nextLine();
-        processFilter(answer, scanner);
+        processAnswer(answer, scanner);
 
     }
 
-    private void processFilter(String answer, Scanner scanner) {
+    private void processAnswer(String answer, Scanner scanner) {
         if (answer.equalsIgnoreCase("n")) {
             printActivities(allActivities);
         } else if (answer.equalsIgnoreCase("y")) {
-            processFilterOptions(scanner);
+            filterOptionsHandler(scanner);
         } else {
             process(scanner);
         }
     }
 
-    private void processFilterOptions(Scanner scanner) {
+    private void filterOptionsHandler(Scanner scanner) {
         printFilterSubMenu();
+        System.out.println("\t\t");
         int filterOption = scanner.nextInt();
         scanner.nextLine();
-        if (filterOption == 1) {
-            ActivityType type = MenuHelpers.getActivityType(scanner);
-            filterByType(type);
-            printActivities(filteredActivities);
+        switch (filterOption) {
+            case 1 -> processFilterByType(scanner);
+            case 2 -> processFilterByTimeInRange(scanner);
+            case 3 -> processFilterByPartOfDescription(scanner);
+            default -> filterOptionsHandler(scanner);
         }
+    }
+
+    private void processFilterByPartOfDescription(Scanner scanner) {
+        System.out.println("Adja mega leírás részletet");
+        String partOfDescription = scanner.nextLine();
+        filterByPartOfDescription(partOfDescription);
+        printActivities(filteredActivities);
+    }
+
+    private void processFilterByTimeInRange(Scanner scanner) {
+        System.out.println("Adja meg az időszak kezdő időpontját: ");
+        LocalDateTime timeFrom = MenuHelpers.getStartTime(scanner);
+        System.out.println("Adja meg az időszak záró időpontját: ");
+        LocalDateTime timeTo = MenuHelpers.getStartTime(scanner);
+        filterByTimeInRange(timeFrom, timeTo);
+        printActivities(filteredActivities);
+    }
+
+    private void processFilterByType(Scanner scanner) {
+        ActivityType type = MenuHelpers.getActivityType(scanner);
+        filterByType(type);
+        printActivities(filteredActivities);
     }
 
     private void filterByType(ActivityType type) {
@@ -58,10 +81,25 @@ public class ListActivityMenuItem implements MenuItem {
                 .filter(activity -> activity.getActivityType().equals(type)).toList();
     }
 
+    private void filterByTimeInRange(LocalDateTime fromTime, LocalDateTime toTime) {
+        filteredActivities = allActivities
+                .stream()
+                .filter(activity -> activity.getStartTime().isAfter(fromTime) && activity.getStartTime().isBefore(toTime))
+                .toList();
+    }
+
+    private void filterByPartOfDescription(String partOfDescription) {
+        filteredActivities = allActivities
+                .stream()
+                .filter(activity -> activity.getDescription().contains(partOfDescription))
+                .toList();
+    }
+
     private void printActivities(List<Activity> activities) {
-        System.out.println("\t\t -------- Összes tevékenység --------");
+        System.out.println("\t\t ------------ Tevékenységek ------------");
+        System.out.println("\t\t ---------------- " + activities.size() + "-db ----------------\n");
         for (Activity actual : activities) {
-            System.out.println('\t' + actual.toString());
+            System.out.println("\t\t" + actual.toString() + "\n");
         }
         System.out.println("\t\t -------------------------------------");
     }
@@ -74,9 +112,9 @@ public class ListActivityMenuItem implements MenuItem {
     }
 
     private void printFilterSubMenu() {
-        System.out.println("1 - Szűrés tipus alapján");
-        System.out.println("2 - Szűrés idősáv alapján");
-        System.out.println("3 - Szűrés leírás részlet alapján");
+        System.out.println("\t\t1 - Szűrés tipus alapján");
+        System.out.println("\t\t2 - Szűrés időszak alapján");
+        System.out.println("\t\t3 - Szűrés leírás részlet alapján");
     }
 
 
